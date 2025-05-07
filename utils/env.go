f960a208ed606f05280ab251b15acb0e2795727d
ALev <me@alev.pro>
2025-05-07 07:13:38 +0200

    FetchEnvMap()

--- utils/env.go
+++ utils/env.go
@@ -21,3 +21,20 @@ func FetchEnvList(name string) []string {
 	}
 	return strings.Split(value, ",")
 }
+
+func FetchEnvMap(name string) map[string]string {
+	value := os.Getenv(name)
+	if value == "" {
+		panic(fmt.Sprintf("%s environment variable is required", name))
+	}
+	pairs := strings.Split(value, ",")
+	result := make(map[string]string)
+	for _, pair := range pairs {
+		kv := strings.Split(pair, "=")
+		if len(kv) != 2 {
+			panic(fmt.Sprintf("Invalid format for %s environment variable", name))
+		}
+		result[kv[0]] = kv[1]
+	}
+	return result
+}
